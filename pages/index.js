7a6b994395523478a6df91f84ca7ba32bb4eb020
rakshith <rakshit.s.gowda@gmail.com>
2021-09-22 22:51:53 +0530

    sort by date

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ import Link from 'next/link'
 
 import Layout from '../components/Layout'
 import Post from '../components/Post'
-
+import { sortByDate } from '../utils'
 export default function HomePage({ posts }) {
-  console.log(posts)
+  // console.log(posts)
   return (
     <Layout>
       <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
@@ -45,7 +45,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   }
 }
